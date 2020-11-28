0c9cecd8607fb2d80640cf8e3ccd9a02d29e086a
ohyland <1oliviahyland@gmail.com>
2020-11-28 21:07:12 +0000

    add alt property to img

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,7 +3,7 @@ import React from "react";
 const Song = ({ currentSong }) => {
   return (
     <div className="song-container">
-      <img src={currentSong.cover} />
+      <img alt={currentSong.name} src={currentSong.cover} />
       <h2>{currentSong.artist}</h2>
       <h2>{currentSong.name}</h2>
     </div>
