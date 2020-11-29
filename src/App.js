0bd398ac5e50cae349784b8c58119fb934aa2f73
Olivia Hyland <52823456+ohyland@users.noreply.github.com>
2020-11-29 17:23:43 +0000

    Selected songs from the library (#8)

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
         setIsPlaying={setIsPlaying}
         currentSong={currentSong}
       />
-      <Library songs={songs} />
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
     </div>
   );
 };
